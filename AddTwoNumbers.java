57a9f43b375a4b028b77468b8e799eb9e40461a2
root <nskwak@hotmail.com>
2020-02-15 08:57:37 -0800

    20200215: RU

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,10 +1,42 @@
-public class AddTwoNumbers {
-
-   public static void main(String[] args) {
-        
-      int num1 = 5, num2 = 15, sum;
-      sum = num1 + num2;
-
-      System.out.println("Sum of these numbers: "+sum);
-   }
+///////////////////////////////////////////////////////////////////////////////////
+// function: LeetCode:Easy 26. Remove Duplicates from Sorted Array
+// Input: [1,1,2]
+// Output: [1,2]
+// Input: [0,0,1,1,1,2,2,3,3,4]
+// Output: [0,1,2,3,4]
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null) return 0;
+        if(nums.length <=1) return nums.length;
+        int i =0;
+        int j= 1;
+        for(; j<nums.length;){
+            if(nums[i] == nums[j]) j++;
+            else nums[++i] = nums[j++];
+        }
+        return ++i;
+    }
 }
+
+/*
+///////////////////////////////////////////////////////////////////////////////////
+// function: AddTwoIntegers
+
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+        
+        int first = 10;
+        int second = 20;
+        double a = 2.345;
+        double b = 325.654;
+        String name = "Grace Kwak";
+
+        int sum = first + second;
+        double sumfloat = a + b;
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("The sum is: " + sumfloat);
+        System.out.println("My name is: " + name);
+    }
+}
+*/
